b0d37922133a4096479b5f6b9ae4b61d151c9991
Imed ZnD <imedzenned@yahoo.fr>
2022-06-08 16:57:46 +0100

    fixing loadcontext test file

--- src/test/kotlin/tn/keyrus/pfe/imdznd/transactionservice/TransactionServiceApplicationTests.kt
+++ src/test/kotlin/tn/keyrus/pfe/imdznd/transactionservice/TransactionServiceApplicationTests.kt
@@ -1,9 +1,16 @@
 package tn.keyrus.pfe.imdznd.transactionservice
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
+import tn.keyrus.pfe.imdznd.transactionservice.dirtyworld.framework.Initializer
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@ContextConfiguration(initializers = [Initializer::class])
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TransactionServiceApplicationTests {
 
     @Test
